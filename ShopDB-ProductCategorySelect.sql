7f810299374f3ef0d44e15d95d0cd7d87b3414fb
Rubanov <35312052+TheIzvorg@users.noreply.github.com>
2022-01-15 11:21:50 +0300

    Убрал сломанные символы

--- ShopDB-ProductCategorySelect.sql
+++ ShopDB-ProductCategorySelect.sql
@@ -1,7 +1,7 @@
 use [ShopDB];
 GO
 
-SELECT p.[Name] as 'Продукт', c.[Name] as 'Категория'
+SELECT p.[Name] as 'Product', c.[Name] as 'Category'
 FROM [ProductCategory] pc
 left join [Category] c ON pc.[CategoryId] = c.[Id]
 left join [Product] p ON pc.[ProductId] = p.[Id]
